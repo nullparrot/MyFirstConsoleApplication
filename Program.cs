f7a89c0ee0cd78b82fdb525004b34bfdfb878dce
nullparrot <github@weist.dev>
2025-04-24 21:28:53 -0700

    Initial Assignment work

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Andrew";
+string location = "The United States";
+Console.WriteLine($"My name is {name}");
+Console.WriteLine($"I am from {location}");
