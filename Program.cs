9e5142a04db04f72008e5c5de4503f0172fef433
nullparrot <github@weist.dev>
2025-04-26 21:49:39 -0700

    Add day of the week to the christmas text

--- Program.cs
+++ Program.cs
@@ -10,25 +10,34 @@ class MyFirstConsoleApplication
         string location = "The United States";
         Console.WriteLine($"My name is {name}");
         Console.WriteLine($"I am from {location}");
+
         // Display Current Date
         DateTime today = DateTime.Today;
-        Console.WriteLine(today.ToString("MM/dd/yyyy"));
+        Console.WriteLine(today.ToString("MM/dd/yyyy "));
+
         // Determine when christmas will be this year and display the days until
         DateTime christmas = DateTime.Parse($"12/25/{today.Year}");
         TimeSpan untilChristmas = christmas.Subtract(today);
-        Console.WriteLine($"There are {untilChristmas.TotalDays} day(s) until Christmas ({christmas.ToString("MM/dd/yyyy")})");
+        Console.WriteLine($"There are {untilChristmas.TotalDays} day(s) until Christmas ({christmas.ToString("dddd, MM/dd/yyyy")})");
 
         //From C# Programming by Rob Miles 2.1.1
+        //Prepare Variables
         double width, height, woodLength, glassArea;
         string widthString, heightString;
+
+        //Get the dimensions of the window
         Console.Write("Window Width: ");
         widthString = Console.ReadLine();
         width = double.Parse(widthString);
         Console.Write("Window Height: ");
         heightString = Console.ReadLine();
         height = double.Parse(heightString);
+
+        //Calculate the amount of wood and glass needed
         woodLength = 2 * (width + height) * 3.25;
         glassArea = 2 * (width * height);
+
+        //Display the results
         Console.WriteLine("The length of the wood is " +
         woodLength + " feet");
         Console.WriteLine("The area of the glass is " +
